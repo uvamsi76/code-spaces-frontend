18237f278ee3f7ca7cd7a5026cd6fca00bebcafc
vamsi <uvamsi76@gmail.com>
2023-11-16 14:32:57 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,12 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
@@ -15,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 export default config
